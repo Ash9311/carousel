9fc83f4bf760a4d79d65b41526b023e5d3a96c97
@Ash9311 <ashutoshmulki7@gmail.com>
2023-06-29 13:28:09 +0530

    fixes

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
 document.addEventListener("DOMContentLoaded",function(){
-    let carouselContainer = document.querySelector("carousel-container");
-    let carousel = document.querySelector("carousel");
-    let prevButton = document.querySelector("prev-btn");
-    let nextButton = document.querySelector("next-btn");
+    let carouselContainer = document.querySelector(".carousel-container");
+    let carousel = document.querySelector(".carousel");
+    let prevButton = document.querySelector(".prev-btn");
+    let nextButton = document.querySelector(".next-btn");
     let images = document.querySelectorAll(".carousel img");
     let currIndex = 0;
 
     //hide all expect the first image
-    for(let i=1;i<images.size();i++){
+    for(var i=1;i<images.length;i++){
         images[i].style.display = "none";
     }
     
     //function to display the current image
     function showImage(index){
         //hide all images
-        for(let i=0;i<images.size();i++){
+        for(var i=0;i<images.length;i++){
             images[i].style.display = "none";
         }
         //display the selected one
-        images[i].style.display="block";
+        images[index].style.display="block";
     }
+
+    prevButton.addEventListener("click",()=>{
+        currIndex--;
+        if(currIndex<0){
+            currIndex = images.length-1;
+        }
+        showImage(currIndex);
+    })
+
+    nextButton.addEventListener("click",()=>{
+        currIndex++;
+        if(currIndex>images.length-1){
+            currIndex=0;
+        }
+        showImage(currIndex);
+    })
 
 })
